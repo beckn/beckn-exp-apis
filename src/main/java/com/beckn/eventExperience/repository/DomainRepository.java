c4027567e38cb677283b3c8672b31d532a9dd02c
avinashetrx <avinash.patil@apna.co>
2022-12-21 19:33:26 +0530

    Removing older codebase

--- src/main/java/com/beckn/eventExperience/repository/DomainRepository.java
+++
@@ -1,7 +0,0 @@
-package com.beckn.eventExperience.repository;
-
-import com.beckn.eventExperience.model.xc.Domain;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DomainRepository extends MongoRepository<Domain, Integer> {
-}
